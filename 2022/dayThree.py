d8d10fa420e4c9f393b262d890dfce0ad874a56f
Ashley Beeson <ashbeeson@hotmail.co.uk>
2022-12-03 16:56:10 +0000

    day three part 2

--- 2022/dayThree.py
+++ 2022/dayThree.py
@@ -1,5 +1,12 @@
 # split string in two/ compare for same char/ find weight of char/add up all weights
 
+def stringCompare(string1, string2):
+    my_str = ''
+    for w in set(string1):
+        if w in string2:
+            my_str += w
+    return my_str
+
 def getCharWeight(char):
     
     points = 1
@@ -27,30 +34,45 @@ with open('2022/data/dayThreeData.txt', 'r') as file:
     
 totalWeight = 0
 weight = []
-count = 1
+count = 0
 bagCheck = []
 bagChackGroup = []
 for bag in data:
-    if count > 3:
-        bagCheck.append(bagChackGroup)
-        bagChackGroup = []
-        count = 1
     bagChackGroup.append(bag)
     firstpart, secondpart = bag[:len(bag)//2], bag[len(bag)//2:]
-    sameChar = ''
-    for fChar in firstpart:
-        for sChar in secondpart:
-            if fChar == sChar:
-                 sameChar = fChar
-    
+    sameChar = stringCompare(firstpart, secondpart)
     weight.append(getCharWeight(sameChar))
-    count+=1
+    count += 1
+    if(count == 3):
+        bagCheck.append(bagChackGroup)
+        bagChackGroup = []
+        count = 0
 
 for w in weight:
     totalWeight += w
 
 
-print(weight)
-print(totalWeight)
+print('Part 1: ' + str(weight))
+print('Part 1:' + str(totalWeight))
 
-print (bagCheck)
+badgeWeight = 0
+badgeSymbol = []
+
+for bagGroup in bagCheck:
+    # compare 1>2, comp 2>3, comp 1>3
+    g1 = stringCompare(bagGroup[0], bagGroup[1])
+    g2 = stringCompare(bagGroup[1], bagGroup[2])
+    g3 = stringCompare(bagGroup[0], bagGroup[2])
+    
+    b1 = stringCompare(g1,g2)
+    b2 = stringCompare(g2,g3)
+    b3 = stringCompare(g1,g3)
+    
+    if b1 == b2 and b2 == b3:
+        badgeSymbol.append(b1)
+
+for badge in badgeSymbol:
+    badgeWeight += getCharWeight(badge)
+    
+print('Part 2: ' + str(badgeSymbol))
+print('Part 2: ' + str(badgeWeight))
